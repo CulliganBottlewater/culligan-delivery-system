c6e1d9edb508b625af2da762b2266e927de0ac8c
CulliganBottlewater <culliganservicescheduling@gmail.com>
2026-04-22 11:13:03 -0600

    Create geocode.js

---
+++ frontend/frontend/css/frontend/shared/frontend/shared/frontend/shared/frontend/shared/geocode.js
@@ -0,0 +1,12 @@
+async function geocodeAddress(address) {
+  const url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(address);
+  const res = await fetch(url, { headers: { 'Accept-Language': 'en' } });
+  const data = await res.json();
+  if (data && data.length > 0) {
+    return {
+      lat: parseFloat(data[0].lat),
+      lon: parseFloat(data[0].lon)
+    };
+  }
+  return null;
+}
